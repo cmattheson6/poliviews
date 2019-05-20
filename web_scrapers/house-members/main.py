287c73c7d7065046cc1e0272ca7b13d1f2058ea0
cmattheson6 <cmattheson6@gmail.com>
2019-05-20 07:33:25 -0400

    Adding main script to house_members

--- web_scrapers/house-members/main.py
+++ web_scrapers/house-members/main.py
@@ -2,7 +2,7 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 from house_members.spiders.house_pols import HousePolsSpider
 
-def main():
+def main(data, context):
     process = CrawlerProcess()
     process.crawl(HousePolsSpider)
     process.start()
