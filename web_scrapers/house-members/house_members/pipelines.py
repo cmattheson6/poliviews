94cf386a2916eb6a078c6319def21054585ac05d
cmattheson6 <cmattheson6@gmail.com>
2019-05-21 08:58:34 -0400

    Revise pipeline for GCS CSV files

--- web_scrapers/house-members/house_members/pipelines.py
+++ web_scrapers/house-members/house_members/pipelines.py
@@ -4,7 +4,6 @@ The rest of the processing will take place in Dataflow.
 """
 
 
-from google.cloud import pubsub
 import logging
 import pandas as pd
 from datetime import date
@@ -14,8 +13,11 @@ import unidecode
 from subprocess import Popen
 import re
 
-file_dirname = '{0}/tmp/house_members'.format(os.path.expanduser('~'))
-file_path = file_dirname + '/house_members_{0}.csv'.format(date.today())
+project_id = 'politics-data-tracker-1'
+bucket_name = 'poliviews'
+pipeline_name = 'house_members'
+file_dirname = 'gs://{0}/{1}/csvs/{2}'.format(project_id, bucket_name, pipeline_name)
+file_path = file_dirname + '/{0}_{1}.csv'.format(date.today())
 rm_old_files = 'rm {0}/*'.format(file_dirname)
 try:
     cmd = Popen(rm_old_files, shell=True).stdout.read()
@@ -24,29 +26,13 @@ except Exception as e:
     print(e)
 
 class PoliticiansPipeline(object):
-    # publisher = pubsub.PublisherClient()
-    # set csv location and open it
-    # file_path = '{0}/tmp/house_pols/house_pols_{1}.csv'.format(os.path.expanduser('~'), date.today())
+    # set csv location and open it\
     f= open(file_path, mode='a+')
     lst = []
     def process_item(self, item, spider):
         """We need to establish a an authorized connection to Google Cloud in order to upload to Google Pub/Sub.
         In order to host the spiders on Github, the service account credentials are housed on the Scrapy platform
         and dynamically created in the script."""
-
-        # Set location of proper publisher topic
-        # project_id = 'politics-data-tracker-1'
-        # topic_name = 'house_pols'
-        # topic_path = self.publisher.topic_path(project_id, topic_name)
-        # data = u'This is a representative in the House.' #Consider how to better use this.
-        # data = data.encode('utf-8')
-        # future = self.publisher.publish(topic_path, data=data,
-        #                   first_name = item['first_name'],
-        #                   last_name = item['last_name'],
-        #                   party = item['party'],
-        #                   state = item['state'],
-        #                   district = item['district'])
-        # logging.info('Published item: {0}'.format(future.result()))
 
         # Add the item as a row in the csv here
         item = {k:unidecode.unidecode(v) for (k,v) in item.items()}
