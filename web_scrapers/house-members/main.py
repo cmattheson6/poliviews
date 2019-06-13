212fe8f9585f7c176cdbb9b5b04ec720a177f3a8
cmattheson6 <cmattheson6@gmail.com>
2019-06-13 09:09:56 -0400

    Testing setups

--- web_scrapers/house-members/main.py
+++ web_scrapers/house-members/main.py
@@ -25,6 +25,12 @@ def main(data, context):
         logging.info('Unable to passively access Google Cloud Storage. Attempting to access credentials ...')
         storage_client = storage.Client.from_service_account_json(gcs_creds)
 
+    process = CrawlerProcess(settings=house_members_settings)
+    logging.info('Initiated CrawlerProcess.')
+    process.crawl(HousePolsSpider)
+    logging.info('Start HousePolsSpider crawl.')
+    process.start()
+
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(blob_name)
     blob.upload_from_filename(tmp_path)
