d9993a45c72b745b214120dcbb365962917524f1
cmattheson6 <cmattheson6@gmail.com>
2019-06-13 08:42:16 -0400

    Testing setups

--- web_scrapers/house-members/main.py
+++ web_scrapers/house-members/main.py
@@ -2,6 +2,11 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 from house_members.spiders.house_pols import HousePolsSpider
 from house_members.settings import house_members_settings
+import logging
+from google.cloud import storage
+import google.cloud.logging as logger
+from house_members.pipelines import tmp_path
+from datetime import date
 
 def main(data, context):
     pass
